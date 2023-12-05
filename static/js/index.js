a36d2e969b87707f3267238d962f11c531276bd5
pablo.guacaneme <pablo.guacaneme@servidiagnostics.com>
2023-12-04 21:10:23 -0500

    Finish Front

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,6 @@
+let mymap;
+let markerLayer = L.layerGroup();
+
 const getCountryCoordinates = async (countryName) => {
     try {
         const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
@@ -10,12 +13,29 @@ const getCountryCoordinates = async (countryName) => {
 
             return { latitude, longitude };
         } else {
-            console.error('No se encontraron datos para el país:', countryName);
-            return null;
+            document.getElementById("mapid").innerHTML = "";
+            document.getElementById("mapid").innerHTML = `<h2>No se encontraron coordenadas para el país: ${countryName}</h2>`;
         }
     } catch (error) {
         console.error('Error al obtener datos de REST Countries API:', error);
-        return null;
+        return;
+    }
+}
+
+const getResultModel = async (dataAPI) => {
+    try {
+        const response = await fetch(`./api/modeloAnalisis`,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(dataAPI)
+        });
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error al obtener datos de REST Countries API:', error);
+        return;
     }
 }
 
@@ -53,87 +73,64 @@ const handleOptionChange = () => {
 
 document.addEventListener('DOMContentLoaded', () => {
     handleOptionChange();
+});
 
-    const form = document.getElementById("myForm");
-    let mymap; 
+document.getElementById("myForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
+    let depth = parseInt(document.getElementById("depth").value, 10) || 0;
+    let optionTime = parseInt(document.getElementById("optionTime").value, 10) || 0;
+    let year = parseInt(document.getElementById("year").value, 10) || 0;
+    let season = parseInt(document.getElementById("season").value, 10) || 0;
+    let month = parseInt(document.getElementById("month").value, 10) || 0;
+    let significance = parseInt(document.getElementById("significance").value, 10) || 0;
+    let state_id = parseInt(document.getElementById("state_id").value, 10) || 0;
 
-    form.addEventListener("submit", function (event) {
-        event.preventDefault();
+    if (optionTime == 1) {
+        season = 0;
+        month = 0
+    } else if (optionTime == 2) {
+        month = 0;
+    } else if (optionTime == 3 || optionTime == 4) {
+        season = 0;
+    }
 
-        let depth = parseInt(document.getElementById("depth").value, 10) || 0;
-        let optionTime = parseInt(document.getElementById("optionTime").value, 10) || 0;
-        let year = parseInt(document.getElementById("year").value, 10) || 0;
-        let season = parseInt(document.getElementById("season").value, 10) || 0;
-        let month = parseInt(document.getElementById("month").value, 10) || 0;
-        let significance = parseInt(document.getElementById("significance").value, 10) || 0;
-        let state_id = parseInt(document.getElementById("state_id").value, 10) || 0;
+    const formData = {
+        depth,
+        optionTime,
+        year,
+        season,
+        month,
+        significance,
+        state_id
+    };
 
-        if (optionTime == 1) {
-            season = 0;
-            month = 0
-        } else if (optionTime == 2) {
-            month = 0;
-        } else if (optionTime == 3 || optionTime == 4) {
-            season = 0;
+    const stateIdElement = document.getElementById("state_id");
+    const countryName = stateIdElement.options[stateIdElement.selectedIndex].text;
+
+    const coordinates = await getCountryCoordinates(countryName);
+    const dataModel = await getResultModel(formData);
+    
+    if (coordinates) {
+        document.getElementById("showInfoModel").innerHTML = "";
+        if (!mymap) {
+            mymap = L.map('mapid').setView([coordinates.latitude, coordinates.longitude], 2);
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '© OpenStreetMap contributors'
+            }).addTo(mymap);
         }
 
-        const formData = {
-            depth,
-            optionTime,
-            year,
-            season,
-            month,
-            significance,
-            state_id
-        };
+        mymap.eachLayer(layer => {
+            if (layer instanceof L.Marker) {
+                mymap.removeLayer(layer);
+            }
+        });
 
-        console.log(formData);
-
-        const stateIdElement = document.getElementById("state_id");
-        const countryName = stateIdElement.options[stateIdElement.selectedIndex].text;
-
-        if (mymap) {
-            mymap.remove();
-        }
-
-        getCountryCoordinates(countryName)
-            .then(coordinates => {
-                if (coordinates) {
-                    console.log(`Coordenadas de ${countryName}:`, coordinates);
-
-                    const mymap = L.map('mapid').setView([coordinates.latitude, coordinates.longitude], 2);
-                    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                        attribution: '© OpenStreetMap contributors'
-                    }).addTo(mymap);
-
-                    const marker = L.marker([coordinates.latitude, coordinates.longitude]).addTo(mymap);
-                    marker.bindPopup(`<b>${countryName}</b><br>Latitud: ${coordinates.latitude}<br>Longitud: ${coordinates.longitude}`).openPopup();
-                }
-            })
-            .catch(error => {
-                console.error("Error al obtener coordenadas:", error);
-            })
-            .finally(() => {
-                // Asegúrate de que este código se ejecute incluso si hay un error
-                fetch("/api/modeloAnalisis", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify(formData)
-                })
-                    .then(response => response.json())
-                    .then(data => {
-                        console.log("Respuesta de la API:", data);
-                    })
-                    .catch(error => {
-                        console.error("Error al enviar datos a la API:", error);
-                    });
-            });
-
-    });
-
-
+        let marker = L.marker([coordinates.latitude, coordinates.longitude]).addTo(mymap);
+        marker.bindPopup(`<b>${countryName}</b><br>Magnitud: ${dataModel.magnitudo}<br>Frecuencia: ${dataModel.frequency}`).openPopup();
+    }else{
+        document.getElementById("showInfoModel").innerHTML = "";
+        document.getElementById("showInfoModel").innerHTML = `<h2>Magnitud: ${dataModel.magnitudo}<br>Frecuencia: ${dataModel.frequency}</h2>`;
+    }
 
 });
 
